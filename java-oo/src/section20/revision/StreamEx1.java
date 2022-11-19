b53a65c18d26733b89b59ebaafa1cbf5a610b7ec
lutch0br <lutch0.br@gmail.com>
2022-11-19 11:34:04 -0300

    Seção 20 - Teste com streams, exercício aula 261 e solução utilizando BufferedWriter para escrever arquivo

---
+++ java-oo/src/section20/revision/StreamEx1.java
@@ -0,0 +1,88 @@
+package section20.revision;
+
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import java.nio.file.Files;
+
+import com.github.javafaker.Faker;
+import util.Screen;
+
+public class StreamEx1 {
+    static final File file = new File("java-oo/src/section20/revision/file.txt");
+    static final Faker faker = new Faker();
+    public static void main(String...args) throws IOException {
+        Locale.setDefault(Locale.US);
+        Screen.clear();
+        write();
+        read();
+
+    }
+
+    static void write(){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))){
+            List<Product> products = getProducts(10);
+
+            for(Product p : products){
+                bw.write(p.toString());
+                bw.newLine();
+            }
+        } catch(IOException e){
+            System.out.printf("%s\n", e.getMessage());
+        }
+    }
+
+    static List<Product> getProducts(int quantity){
+        List<Product> products = new ArrayList<>();
+
+        for(int i = 0; i < quantity; i++){
+            String name = faker.commerce().productName();
+            Date validity = faker.date().future(60, TimeUnit.DAYS);
+            double price = faker.number().randomDouble(2, 10, 1000);
+            products.add(new Product(name, price, validity));
+        }
+
+        return products;
+    }    
+
+    static void delete(){}
+
+    static void read() throws IOException {
+        Files.lines(file.toPath())
+            .collect(Collectors.toList())
+            .forEach(System.out::println);
+    }
+}
+
+class Product{
+    static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    String name;
+    Double price;
+    Date validity;
+
+    public Product(String name, Double price, Date validity){
+        this.name = name;
+        this.validity = validity;
+        this.price = price;
+    }
+
+    public String toString(){
+        String validitySTR = sdf.format(validity);
+
+        return String.format("%s,%s,%.2f", name, validitySTR, price);
+    }
+}
--- java-oo/src/section20/revision/StreamTest.java
+++ java-oo/src/section20/revision/StreamTest.java
@@ -7,7 +7,10 @@ import java.text.ParseException;
 import java.util.Date;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.stream.Stream;
 import java.util.Comparator;
 import java.util.function.Predicate;
 import java.util.ArrayList;
@@ -19,19 +22,36 @@ import util.Screen;
 /**
 * Revisão da utilização de função como args
 */
-public class FunctionAsArg {
+public class StreamTest {
 
     final static Faker faker = new Faker();
 
     public static void main(String...args) throws ParseException {
+        Locale.setDefault(Locale.US);
         Screen.clear();
-        List<Product> products = getProducts(10);
+
+        List<Product> products = getProducts(20);
         
         System.out.println("Original list");
-        products.forEach(System.out::println);
-
-        System.out.println("----------");
-        System.out.println("Filtered Sum");
+        products
+            .stream()
+            .collect(Collectors.toList())
+            .forEach(System.out::print);
+        
+        System.out.println("--------------");        
+        System.out.println("Stream from var args");
+        Stream ps2 = Stream.of(
+            "Synergistic Leather Computer", "Mediocre Steel Lamp");        
+        System.out.println(Arrays.toString(ps2.toArray()));
+        
+        System.out.println("--------------");
+        System.out.println("New List added 10% price");
+        products
+            .stream()
+            .filter(p -> p.price < 500)
+            .map(p -> new Product(p.name, p.price * 1.1, p.validity))
+            .collect(Collectors.toList())
+            .forEach(System.out::print);
 
     }
 
@@ -39,10 +59,10 @@ public class FunctionAsArg {
         List<Product> products = new ArrayList<>();
 
         for(int i = 0; i < quantity; i++){
-            String name = faker.name().fullName();
-            Date birth = faker.date().future(60, TimeUnit.DAYS);
+            String name = faker.commerce().productName();
+            Date validity = faker.date().future(60, TimeUnit.DAYS);
             double price = faker.number().randomDouble(2, 10, 1000);
-            products.add(new Product(name, price, birth));
+            products.add(new Product(name, price, validity));
         }
 
         return products;
@@ -64,6 +84,6 @@ class Product{
     public String toString(){
         String validitySTR = sdf.format(validity);
 
-        return String.format("%-25s %s %7.2f", name, validitySTR, price);
+        return String.format("%s, %.2f\n", name, price);
     }
 }
