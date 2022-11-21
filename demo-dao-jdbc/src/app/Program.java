5f7781a931cff0be650c809fdb9383e0cfdcb1a4
lutch0br <lutch0.br@gmail.com>
2022-11-21 20:15:17 -0300

    Department Entity Class

---
+++ demo-dao-jdbc/src/app/Program.java
@@ -0,0 +1,14 @@
+package app;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department d = new Department(1, "TI");
+		System.out.println(d);
+
+	}
+
+}
