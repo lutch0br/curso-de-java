2a390d0032a682ac3741c02fdb09a36eee1e9dfb
lutch0br <lutch0.br@gmail.com>
2022-11-21 23:08:26 -0300

    Seller entity and Interfaces

--- demo-dao-jdbc/src/app/Program.java
+++ demo-dao-jdbc/src/app/Program.java
@@ -1,13 +1,25 @@
 package app;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
 		
 		Department d = new Department(1, "TI");
 		System.out.println(d);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Seller s = new Seller(
+				1, "Bea", "bea@email.com", sdf.parse("05/07/1995"), 2500.0);
+		System.out.println(s);
 
 	}
 
---
+++ demo-dao-jdbc/src/model/entities/Seller.java
@@ -0,0 +1,23 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Seller implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
+	
+}
