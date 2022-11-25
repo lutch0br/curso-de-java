db795293b62e62abb9c9f7df70593f25553bfed9
lutch0br <lutch0.br@gmail.com>
2022-11-25 20:09:18 -0300

    Componentes GUI I - My first JOptionPane

---
+++ componentes-gui-i/src/app/ProgramAdd.java
@@ -0,0 +1,20 @@
+package app;
+
+import javax.swing.JOptionPane;
+
+public class ProgramAdd {
+
+	public static void main(String[] args) {
+		String firstNumber = JOptionPane.showInputDialog("Entrer first integer: ");
+		String secondNumber = JOptionPane.showInputDialog("Entrer second integer: ");
+		
+		int number1 = Integer.parseInt(firstNumber);
+		int number2 = Integer.parseInt(secondNumber);
+		
+		int sum = number1 + number2;
+		
+		JOptionPane.showMessageDialog(null, "The sum is " + sum, "Sum of two integers", JOptionPane.PLAIN_MESSAGE);
+	}
+
+}
+
